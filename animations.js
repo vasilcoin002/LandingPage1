9663af76f26f5fcb6a5ab2fea85dbe6b641e0528
vasilcoin002 <vasilcoin002@gmail.com>
2023-06-22 01:50:17 +0300

    Created Start Animation

---
+++ animations.js
@@ -0,0 +1,5 @@
+const text = document.querySelector('.hi__text')
+setTimeout(() => {text.classList.add('hi__text_animated')}, 1000)
+
+const images = document.querySelector('.hi__images')
+setTimeout(() => {images.classList.add('hi__images_animated')}, 2000)
